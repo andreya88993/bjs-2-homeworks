a8438cc5244a92df025bbc5891178a6eaf21755d
Андрей Артамонов <artamonov88993@yandex.ru>
2023-04-12 21:45:37 +0300

    Новые изменения

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -5,25 +5,10 @@
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let genders = users.filter(i => i.gender === gender); // фильтруюсь по значению gender
-  
-  let allAge = genders.map(g => g.age); // формирую новый массив по age
- let sumAllAge = allAge.reduce((f, rez) => rez + f, 0);// вычисляю сумму возраста
-  
-  // return sumAllAge / allAge.length; // рассчитываю средний возраст
-  
-  if (users.length === 0) {
-     return 0;
-    }
-  for (i = 0; i < users.length; i++) {
-    
-    if (users[i].gender !== gender) {
-    0;
-    } else {
-      return sumAllAge / allAge.length;
-    }
-  }
-  return 0;
- 
- 
+    return users.filter(user => user.gender === gender).map(ages => ages.age).reduce((acc, age, index, arr) => {if (index === arr.length - 1) {return (acc + age) / arr.length;
+} else {
+   return acc + age;
 }
+}, 0);
+}
+
