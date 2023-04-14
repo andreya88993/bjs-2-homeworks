34012d1d79637dd4017ef41759017a3d8173fc0b
Андрей Артамонов <artamonov88993@yandex.ru>
2023-04-14 22:19:14 +0300

    Добавил решение по объектам

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+  let student1 = new Student("Саша", "Мужчина", 20);
+  let student2 = new Student("Алеша", "Мужчина", 16);
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+  Student.prototype.setSubject = function (subjectName) {
+    return this.subject = subjectName;
+    }
+
+    Student.prototype.addMarks = function (...marks) {
+        if (!this.marks) {
+          return 'отчислен';
+        }
+        this.marks.push(...marks);
+          }
 
 Student.prototype.getAverage = function () {
-  
+   if(!this.marks || this.marks.length === 0) {
+    return 0;
+   }
+   let sum = this.marks.reduce(function (acc, curr) {
+    return acc + curr;
+  }, 0);
+  return sum / this.marks.length;
 }
 
+
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
